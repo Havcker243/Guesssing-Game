62d6600f7e65ee2b753845266e928beaaba0cc9f
Dolapo <oadegbesan25@my.whitworth.edu>
2023-09-26 12:10:05 -0700

    update

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-// Your solution goes here 
-function playGuessingGame(numToGuess, totalGuesses = 10) {
-    let promptText = "Enter a number between 1 and 100.";
-    let guessCount = 0;
-    let guess = 0;
-    let isValidInput = false;
-    while (guessCount < totalGuesses) {
-        isValidInput = false;
-        while (!isValidInput) {
-            guess = prompt(promptText);
-            if (guess === null) {
-                return 0;
-            }
-            if (isNaN(guess) || guess === "") {
-                promptText = "Please enter a number.";
-            } else {
-                isValidInput = true;
-                guess = Number(guess);
-                guessCount++;
-                if (guess === numToGuess) {
-                    return guessCount;
-                } else if (guess < numToGuess) {
-                    promptText = `${guess} is too small. Guess a larger number.`;
-                } else if (guess > numToGuess) {
-                    promptText = `${guess} is too large. Guess a smaller number.`;
-                }
-            }
+let numToGuess = Math.floor(Math.random() * 100) + 1;
+let totalGuesses = 10;
+let guessCount = 0;
+
+function checkGuess() {
+    let userGuess = document.getElementById('userGuess').value;
+
+    if (isNaN(userGuess) || userGuess === "") {
+        document.getElementById('response').textContent = "Please enter a number.";
+    } else {
+        guessCount++;
+        userGuess = Number(userGuess);
+
+        if (userGuess === numToGuess) {
+            document.getElementById('response').textContent = 'Congratulations! You guessed it right in ' + guessCount + ' tries.';
+        } else if (guessCount === totalGuesses) {
+            document.getElementById('response').textContent = 'Sorry, you\'ve used up all your guesses. The correct number was ' + numToGuess;
+        } else if (userGuess < numToGuess) {
+            document.getElementById('response').textContent = userGuess + ' is too small. Guess a larger number. You have ' + (totalGuesses - guessCount) + ' guesses left.';
+        } else if (userGuess > numToGuess) {
+            document.getElementById('response').textContent = userGuess + ' is too large. Guess a smaller number. You have ' + (totalGuesses - guessCount) + ' guesses left.';
         }
     }
-    return 0;
 }
 
 
